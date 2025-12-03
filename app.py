5ac2904f0e8547f822cdc66922ddd12b953ae971
HaeengIn <haeengin@gmail.com>
2025-12-03 06:19:10 +0000

    Delete unnecessary blanks / Add function that opens wiki page, prints info, and exits the app

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import process, os
-
+import process, os, sys, webbrowser
 
 def clear_console():
     os.system("cls" if os.name == "nt" else "clear")
-
 
 # Run the single process
 def run_single_process():
@@ -35,7 +33,6 @@ def run_single_process():
                 break
         main()
 
-
 # Run the multiple process
 def run_multiple_process():
     print("[ Multiple Process ]")
@@ -66,21 +63,38 @@ def run_multiple_process():
                 break
         main()
 
-
 # Open Wiki page
 def open_wiki():
-    pass
-
+    webbrowser.open("https://messy-yak-716.notion.site/Image-Tweaks-Official-Wiki-2b96afdf1918807f8063f42a75ba5cbd")
+    main()
 
 # Open Info page
 def open_info():
-    pass
-
+    print("[ Image Tweaks ]\n" \
+    "Version: Alpha 1.0.0\n" \
+    "Develop: HaeengIn\n" \
+    "Last Update: 2025-12-03\n" \
+    "\n" \
+    "Press any key to return to the main menu.")
+    input("> ")
+    clear_console()
+    main()
 
 # Exit the app
 def exit_app():
-    pass
-
+    while True:
+        print("Do you really want to exit the app? (Y/N)")
+        confirm = input("> ").strip().lower()
+        if confirm in ["y", "n"]:
+            break
+        else:
+            print("Invalid Input. Please enter Y or N.")
+    
+    if confirm == "y":
+        sys.exit()
+    elif confirm == "n":
+        clear_console()
+        main()
 
 # Run the app
 def main():
@@ -94,13 +108,21 @@ def main():
             if process_type == "single":
                 clear_console()
                 run_single_process()
-            else:
+            elif process_type == "multiple":
                 clear_console()
                 run_multiple_process()
+            elif process_type == "wiki":
+                clear_console()
+                open_wiki()
+            elif process_type == "info":
+                clear_console()
+                open_info()
+            elif process_type == "exit":
+                clear_console()
+                exit_app()
             break
         else:
-            print("Invalid Input. Please enter 'Single' or 'Multiple'")
-
+            print("Invalid Input.")
 
 if __name__ == "__main__":
     main()
